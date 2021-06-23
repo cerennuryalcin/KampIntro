b71a7827567ef19695ed19069ae0a532a0297f76
Hp <Hp@DESKTOP-LETDBRB>
2021-06-23 17:19:13 +0300

    Add project files.

---
+++ ClasIntro/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClasIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "c#";
+            kurs1.Egitmen = "engin demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "java";
+            kurs2.Egitmen = "kerem varış";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "python";
+            kurs3.Egitmen = "cerennur yalçın";
+            kurs3.IzlenmeOrani = 86;
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+class kurs
+{
+    public string KursAdi { get; set; }
+    public string Egitmen { get; set; }
+    public int İzlenmeOrani { get; set; }
+
+
+   
